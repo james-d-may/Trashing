48ac1efb74ab4846d27f000f64e56cfe287d6a20
Emil <eculic17@gmail.com>
2014-05-28 18:19:10 +0100

    updated forms to work better on mailchimp

--- paypalipn.php
+++ paypalipn.php
@@ -79,13 +79,15 @@ if (strcmp ($res, "VERIFIED") == 0) {
     if (strcmp ($payment_status, "Completed") == 0) {
       // Send Google Form
       $codes = file('codes.txt'); 
-      $ref_owned = substr($codes[0], 0, 9);
+      $ref_owned = "http://www.trashing.me?ref=";
+      $ref_owned .= s
       $codes[0] = "";
 
       $fp = fopen('codes.txt', 'w'); 
       fwrite($fp, implode($codes));
 
       $req = "";
+      $req .= "&entry.1344218879=no";
       $req .= "&entry.96823989=no";
       $req .= "&entry.919457879=" . "$last_name";
       $req .= "&entry.1063042359=" . "$first_name";
